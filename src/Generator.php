4822c37c0370aa4f7ab784d49d07858d5dee56f3
Paul Rock <paul@drteam.rocks>
2020-04-08 22:31:04 +0300

    rendering of section added

--- src/Generator.php
+++ src/Generator.php
@@ -23,12 +23,37 @@ class Generator implements GeneratorInterface
     }
 
     /**
+     * Render section by parameters in array
+     *
+     * @param \XL2TP\Section $section
+     *
+     * @return string
+     */
+    private function render(Section $section): string
+    {
+        $name = $section->section;
+        if ($section->section !== 'global') {
+            $name .= $section->suffix ? ' ' . $section->suffix : '';
+        }
+
+        $result = "[$name]" . PHP_EOL;
+        foreach ($section->parameters as $key => $val) {
+            $result .= "$key = " . (is_numeric($val) ? $val : '"' . $val . '"') . PHP_EOL;
+        }
+        return $result . PHP_EOL;
+    }
+
+    /**
      * Generate L2TP configuration by parameters from memory
      *
      * @return string
      */
     public function generate(): string
     {
-        // TODO: Implement generate() method.
+        $result = '';
+        foreach ($this->config->sections as $section) {
+            $result .= $this->render($section);
+        }
+        return $result;
     }
 }
