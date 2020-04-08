948c8b332edbc62a4ee7aa1bf54a360adfbdb708
Paul Rock <paul@drteam.rocks>
2020-04-08 22:31:25 +0300

    examples updated, render call added

--- examples/test.php
+++ examples/test.php
@@ -4,8 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $obj = new \XL2TP\Config();
 
 $obj->global->port = 123;
-$obj->lnsDefault->port = 123;
-$obj->lns('test')->port = 123;
-$obj->lac()->port = 123;
+$obj->lns->lac = 'test';
+$obj->lns('test')->exclusive = 'yes';
+$obj->lac()->redial = 123;
 
-dd($obj);
+// dd($obj);
+echo $obj->generate();
