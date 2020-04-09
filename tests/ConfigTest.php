a74d93224b43efb4110fd81da1b025dc3215a313
Compolomus <compolom@gmail.com>
2020-04-09 08:49:46 +0200

    Settings and prepare tests

---
+++ tests/ConfigTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace XL2TP\Tests;
+
+use XL2TP\Config;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+
+    public function test__get()
+    {
+
+    }
+
+    public function test__set()
+    {
+
+    }
+
+    public function testGenerate()
+    {
+
+    }
+
+    public function testGlobal()
+    {
+
+    }
+
+    public function testSection()
+    {
+
+    }
+
+    public function test__call()
+    {
+
+    }
+
+    public function testLac()
+    {
+
+    }
+
+    public function testLns()
+    {
+
+    }
+
+    public function test__isset()
+    {
+
+    }
+}
---
+++ tests/GeneratorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace XL2TP\Tests;
+
+use XL2TP\Generator;
+use PHPUnit\Framework\TestCase;
+
+class GeneratorTest extends TestCase
+{
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testGenerate()
+    {
+
+    }
+}
